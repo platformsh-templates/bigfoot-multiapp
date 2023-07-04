e786cf510f67b45f44bb4a572787c623ca13637f
Florent Huck <flovntp@gmail.com>
2023-07-04 11:54:07 +0200

    missing POST use

--- api/src/Entity/BigFootSighting.php
+++ api/src/Entity/BigFootSighting.php
@@ -8,6 +8,7 @@ use ApiPlatform\Metadata\Get;
 use ApiPlatform\Metadata\GetCollection;
 use ApiPlatform\Metadata\Patch;
 use ApiPlatform\Metadata\Put;
+use ApiPlatform\Metadata\Post;
 use App\Repository\BigFootSightingRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
--- api/src/Entity/Comment.php
+++ api/src/Entity/Comment.php
@@ -8,6 +8,7 @@ use ApiPlatform\Metadata\Get;
 use ApiPlatform\Metadata\GetCollection;
 use ApiPlatform\Metadata\Patch;
 use ApiPlatform\Metadata\Put;
+use ApiPlatform\Metadata\Post;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Serializer\Annotation\Groups;

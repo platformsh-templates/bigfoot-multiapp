18dd13143e4aad45fc92728bc44adbb68e7cea4d
Florent Huck <flovntp@gmail.com>
2023-11-16 17:08:24 +0100

    align with psh version

--- api/config/bundles.php
+++ api/config/bundles.php
@@ -17,5 +17,4 @@ return [
     Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
     ApiPlatform\Symfony\Bundle\ApiPlatformBundle::class => ['all' => true],
     Symfony\Bundle\MercureBundle\MercureBundle::class => ['all' => true],
-    DAMA\DoctrineTestBundle\DAMADoctrineTestBundle::class => ['test' => true],
 ];

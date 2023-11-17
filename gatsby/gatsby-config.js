a744d39255a82b0ef80cbe29075bb98ca9f683a6
Florent Huck <flovntp@gmail.com>
2023-11-17 10:28:11 +0100

    Gatsby: debug

--- gatsby/gatsby-config.js
+++ gatsby/gatsby-config.js
@@ -24,13 +24,14 @@ if ('PLATFORM_ROUTES' in process.env) {
       .map(([key, value]) => key)
   // index = 4;
   pathPrefix = '/site'
-  siteUrl = `${result[0]}`;
+  siteUrl = `${result[0]}site`;
   backendURL = `${result[0]}api/graphql`
 
 } else {
   console.log('Running locally.');
 }
 
+console.log(siteUrl)
 console.log(backendURL)
 
 module.exports = {
